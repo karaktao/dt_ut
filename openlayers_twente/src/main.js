1b841c8a1afb737da75e1b354915329f31dbf0b9
karaktao <1317113320@qq.com>
2025-04-10 16:45:59 +0200

    添加berth

--- openlayers_twente/src/main.js
+++ openlayers_twente/src/main.js
@@ -6,24 +6,7 @@ import Map from 'ol/Map.js';
 import View from 'ol/View.js';
 import TileLayer from 'ol/layer/Tile.js';
 import OSM from 'ol/source/OSM.js';
-
-
-
-// const map = new Map({
-//   view: new View({
-//     center: [694118.1989320087, 6829154.600371235],
-//     zoom: 16,
-//     // projection: "EPSG:4326",
-//     //Delden:743345.6427273116,6844779.718830286
-//     //Hengelo:757391.5712651528,6844752.293851171
-//   }),
-//   layers: [
-//     new TileLayer({
-//       source: new OSM(),
-//     }),
-//   ],
-//   target: 'map',
-// });
+import XYZ from 'ol/source/XYZ';
 
 
 
@@ -35,17 +18,23 @@ const view = new View({
   //Hengelo:757391.5712651528,6844752.293851171
 });
 
-const layer = new TileLayer({
+const osmLayer = new TileLayer({
   source: new OSM(),
 })
+
+const esriLayer = new TileLayer({
+  source: new XYZ({
+    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+  })
+});
 
 const map = new Map({
   target: 'map',
   view,
-  layers: [layer]
+  layers: [osmLayer, esriLayer]
 })
 
-//点击事件，选择按钮元素
+//更换地点
 const btnEefde = document.querySelector('.btnsEefde button');
 if (btnEefde) {
   btnEefde.onclick = function () {
@@ -66,3 +55,19 @@ if (btnHengelo) {
     view.setCenter([757391.5712651528,6844752.293851171]);
   };
 }
+
+//更换地图
+const streetMapBtn = document.querySelector('.btnsStreetmap button');
+const satelliteMapBtn = document.querySelector('.btnsSatellitemap button');
+
+
+satelliteMapBtn.addEventListener('click', () => {
+  osmLayer.setVisible(false);
+  esriLayer.setVisible(true);
+});
+
+streetMapBtn.addEventListener('click', () => {
+  osmLayer.setVisible(true);
+  esriLayer.setVisible(false);
+});
+
